ff8de7c72d44b6d93a2138a4593be56984a64446
shitalmarathe <shitalmarathe57@gmail.com>
2025-02-18 17:12:48 +0000

    create express api

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hey there");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server fired up 🔥 on PORT: ${PORT}`);
+});
