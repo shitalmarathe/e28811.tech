35222c5a9ef20344ff88dc16052aafd43829b696
shitalmarathe <shitalmarathe57@gmail.com>
2025-02-19 09:48:37 +0000

    create ejs template for homepage

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hey there");
+  res.render("homepage", { name: "Codespace" });
 });
+
+app.get("/about", (req, res) => {
+    res.render("homepage", { name: "About us" });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server fired up 🔥 on PORT: ${PORT}`);
