c987246c2b146ec9fa64cf0bc948a4ef554d3b94
shitalmarathe <shitalmarathe57@gmail.com>
2025-02-19 10:52:52 +0000

    fix changes

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+  const app = express();
+  app.set("view engine", "ejs");
+  app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+  app.get("/", (req, res) => {
   res.render("homepage");
 });
 
-app.get("/login", (req, res) => {
+  app.get("/login", (req, res) => {
     res.render("login");
   });
+
+
+  app.post("/login", (req, res) => {
+    res.send("Thanks, you're now logged in!");
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Server fired up 🔥 on PORT: ${PORT}`);
