ea98ec0c801d0c62f81c5df73be4a595fdc941f7
shitalmarathe <shitalmarathe57@gmail.com>
2025-02-20 11:30:00 +0000

    add validation for registration

--- index.js
+++ index.js
@@ -14,8 +14,40 @@ const PORT = process.env.PORT || 3000;
 
 // User Registration Starts
 app.post("/register", (req, res) => {
-  console.log(req.body);
-  return res.send("Thanks for registering");
+  let { username, password } = req.body;
+  const errors = [];
+  
+  username =  username.trim();
+  if (typeof username !== "string") username = "";
+  if (typeof password !== "string") password = "";
+
+  // Username Validation
+  if (!username) {
+    errors.push("Username is required");
+  }
+  if (username && username.length < 4) {
+    errors.push("Username must contain atleast 4 characters");
+  }
+  if (username && username.length > 12) {
+    errors.push("Username must not exceed 12 characters");
+  }
+  if (username && !username.match(/^[a-zA-Z0-9]+$/)) {
+    errors.push("Username can't contain special characters");
+  }
+
+  // Password Validation
+  if (!password) {
+    errors.push("Password is required");
+  }
+  if (password && password.length < 6) {
+    errors.push("Password must contain atleast 6 characters");
+  }
+  if (password && password.length > 20) {
+    errors.push("Password must not exceed 20 characters");
+  }
+
+  
+  return res.send(errors);
 });
 // User Registration Ends
 
